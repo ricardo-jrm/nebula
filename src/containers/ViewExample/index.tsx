81e24d6aefe801bff65ca6ba895a7d848eb99b26
l1b3r_- <liber@ricardojrm.com>
2022-01-17 07:00:20 +0000

    fix: example

--- src/containers/ViewExample/index.tsx
+++ src/containers/ViewExample/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ExampleComponent } from '../../components/_example/example';
+import { ExampleComponent } from '../../components/Example/example';
 
 /**
  * ViewExampleProps
--- src/containers/example/index.tsx
+++ src/containers/example/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ExampleComponent } from '../../components/_example';
+import { ExampleComponent } from '../../components/Example';
 
 /**
  * HomeViewProps
