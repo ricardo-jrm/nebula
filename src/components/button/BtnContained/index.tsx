8a3e948e3228b8fb3fbace13bb5bc40c9add9b5d
ricardojrmcom <liber@ricardojrm.com>
2022-05-27 05:43:25 +0100

    feat(buttons): add sx prop

--- src/components/button/BtnContained/index.tsx
+++ src/components/button/BtnContained/index.tsx
@@ -2,11 +2,15 @@ import React, { FC } from 'react';
 import { Button, ButtonProps } from '@mui/material';
 
 export interface BtnContainedProps
-  extends Pick<ButtonProps, 'children' | 'color'> {}
+  extends Pick<ButtonProps, 'children' | 'sx' | 'color'> {}
 
-export const BtnContained: FC<BtnContainedProps> = ({ children, color }) => {
+export const BtnContained: FC<BtnContainedProps> = ({
+  children,
+  sx,
+  color,
+}) => {
   return (
-    <Button color={color} variant="contained">
+    <Button color={color} variant="contained" sx={sx}>
       {children}
     </Button>
   );
--- src/components/button/BtnText/index.tsx
+++ src/components/button/BtnText/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { Button, ButtonProps } from '@mui/material';
 
-export interface BtnTextProps extends Pick<ButtonProps, 'children' | 'color'> {}
+export interface BtnTextProps
+  extends Pick<ButtonProps, 'children' | 'sx' | 'color'> {}
 
-export const BtnText: FC<BtnTextProps> = ({ children, color }) => {
+export const BtnText: FC<BtnTextProps> = ({ children, sx, color }) => {
   return (
-    <Button color={color} variant="text">
+    <Button color={color} variant="text" sx={sx}>
       {children}
     </Button>
   );
